d39ff26042f183d21c75685b59c5e8be373afe06
Segun David <segdavid03@gmail.com>
2024-12-22 07:22:23 +0100

    feat: Setup increase and decrease cart item functionality

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,27 @@
 "use strict";
 
 const iconCart = document.querySelector(".icon__cart");
+const decreaseItem = document.querySelector(".decrease__item");
+const increaseItem = document.querySelector(".increase__item");
+const count = document.querySelector(".count");
+
+let countValue = 0;
 
 // Open cart functionality
 iconCart.addEventListener("click", () => {
   alert("added to cart");
+});
+
+// Increase cart item
+increaseItem.addEventListener("click", () => {
+  countValue++;
+  count.innerHTML = countValue;
+});
+
+// Decrease cart item
+decreaseItem.addEventListener("click", () => {
+  if (countValue > 0) {
+    countValue--;
+    count.innerHTML = countValue;
+  }
 });
