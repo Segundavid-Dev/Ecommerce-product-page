de1b815a7c315cb188a004c55b8493aaef98cb6d
Segun David <segdavid03@gmail.com>
2024-12-23 22:50:15 +0100

    feat: calcualate total price for each value

--- assets/app.js
+++ assets/app.js
@@ -6,13 +6,23 @@ const increaseItem = document.querySelector(".increase__item");
 const count = document.querySelector(".count");
 const modal = document.querySelector(".modal");
 const cartCheckout = document.querySelector(".cart__checkout");
-const addToCart = document.querySelector(".add_to_cart");
+let addToCart = document.querySelector(".add_to_cart");
+const delete_item = document.querySelector(".delete");
+const cartInfo = document.querySelector(".cart__info");
+let totalItem = document.querySelector(".total__item");
+let totalPrice = document.querySelector(".total__price");
+const disCountPrice = 125;
 let countValue = 0;
 
 // Increase cart item
 increaseItem.addEventListener("click", () => {
   countValue++;
   count.innerHTML = countValue;
+  totalItem.innerHTML = countValue;
+
+  // calculate total price
+  const calculatedTotal = disCountPrice * countValue;
+  totalPrice.innerHTML = calculatedTotal;
 });
 
 // Decrease cart item
@@ -20,12 +30,17 @@ decreaseItem.addEventListener("click", () => {
   if (countValue > 0) {
     countValue--;
     count.innerHTML = countValue;
+    totalItem.innerHTML = countValue;
   }
 });
 
 // Open modal message
 iconCart.addEventListener("click", () => {
-  modal.classList.add("modal__open");
+  if (countValue === 0) {
+    alert("Empty Cart ");
+  } else {
+    modal.classList.add("modal__open");
+  }
 });
 
 // Checkout modal message
@@ -34,11 +49,25 @@ cartCheckout.addEventListener("click", () => {
 });
 
 // Add to cart functionality
-
 addToCart.addEventListener("click", () => {
-  if (countValue == 0) {
-    alert("Add item to cart!");
+  if (countValue === 0) {
+    alert("your cart is empty");
   } else if (countValue > 0) {
     modal.classList.add("modal__open");
   }
+});
+
+// check if items have been added to cart to dynamically display modal messages
+// addToCart = false;
+// if (
+//   addToCart.addEventListener("click", () => {
+//     console.log("clicked event happen");
+//   })
+// ) {
+//   alert("good");
+// }
+
+// still need some refactoring!!!
+delete_item.addEventListener("click", () => {
+  alert("removed items from cart");
 });
